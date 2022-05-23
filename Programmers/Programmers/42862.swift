4cddea602d6870480352d61959015587adf6cea6
bibi <bibi6666667@kakao.com>
2022-05-23 22:58:53 +0900

    체육복

--- Programmers/Programmers/42862.swift
+++ Programmers/Programmers/42862.swift
@@ -9,8 +9,8 @@ import Foundation
 
 func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int { // n : 학생 수, lost : 도난 번호, reserve : 여분 번호
     var answer = 0
-    var reserveStudents = reserve
-    var lostStudents = lost
+    var reserveStudents = reserve.sorted()
+    var lostStudents = lost.sorted()
     
     answer += (n - reserveStudents.count - lostStudents.count) // 여분x 도난x 학생
     answer += reserveStudents.count // 여분이 있는 학생은 무조건 수강 가능
@@ -24,13 +24,13 @@ func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int { // n : 학생 수
         }
         if lostStudents.contains(reserveStudent - 1) {
             if let index = lostStudents.firstIndex(of: reserveStudent - 1) {
-                print("\(reserveStudent)학생이 앞의 \(index)학생에게 체육복 빌려줌")
+                print("\(reserveStudent)학생이 앞의 학생에게 체육복 빌려줌")
                 lostStudents.remove(at: index)
             }
             answer += 1
         } else if lostStudents.contains(reserveStudent + 1) {
             if let index = lostStudents.firstIndex(of: reserveStudent + 1) {
-                print("\(reserveStudent)학생이 뒤의 \(index)학생에게 체육복 빌려줌")
+                print("\(reserveStudent)학생이 뒤의 학생에게 체육복 빌려줌")
                 lostStudents.remove(at: index)
             }
             answer += 1
@@ -39,3 +39,21 @@ func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int { // n : 학생 수
     
     return answer
 }
+
+// 좋아 보였던 풀이 - https://babbab2.tistory.com/108
+//
+//func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+//    var lostSet = Set(lost).subtracting(reserve)
+//    let reserveSet = Set(reserve).subtracting(lost)
+//
+//    for reserve in reserveSet {
+//        if lostSet.contains(reserve - 1) {      // 내 앞 번호 학생이 체육복이 없는지 확인
+//            lostSet.remove(reserve - 1)
+//            continue
+//        }
+//        if lostSet.contains(reserve + 1) {      // 내 뒷 번호 학생이 체육복이 없는지 확인
+//            lostSet.remove(reserve + 1)
+//        }
+//    }
+//    return (n - lostSet.count)
+//}
